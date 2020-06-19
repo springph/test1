8b9114566f09d4a2972349c75ad74e95c89c2fe8
springph <61903186+springph@users.noreply.github.com>
2020-06-19 17:41:27 +0800

    Update Test1.java

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -4,5 +4,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("github");
 		System.out.println("shabi");
+                System.out.println("tedu");
+
+
 	}
 }
